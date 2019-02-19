59ffe9c92d684f7c9ff631a117338d57197d1548
pavel-b-kr12 <kr16@ukr.net>
2019-02-19 20:58:37 +0200

    Add files via upload

---
+++ enable (uncomment) marked cases.cs
@@ -0,0 +1,28 @@
+string s=
+@"   
+//case 2: ..... //not168
+case 4:   //not328
+//case 4:s
+";
+
+string commentMark="not168";
+
+int lastN=0;
+s=Replace(
+ s,
+ @".*",
+ m=>
+ {
+   if(m.Value.Contains(commentMark)) 
+   {
+	return Replace(m.Value,".*case ","case ");
+   }
+   else 
+   {
+	return m.Value;
+   }
+ }
+);
+
+
+WriteLine(s);
